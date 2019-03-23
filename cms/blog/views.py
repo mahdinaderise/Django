5667b64c7c6ef5067060bc6ffb221923a320c9ec
mahdi <m.naderi002@hotmail.com>
2019-03-23 10:23:25 -0400

    ezafe kardan adres
    addres ha ezafeh shod

--- cms/blog/views.py
+++ cms/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import post
 # Create your views here.
+#ساخت یک ویو برای نمایش لیست پست ها
+def list_of_post(request):
+    Post=post.Objects.all()
+    template='blog/post/list_of_post.html'
+    context={'Post':Post}
+    return render(request,template,context)
